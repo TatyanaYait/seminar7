c00254de4fd11ce33a262766b2acf59034a02ee5
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-09 01:50:31 +0600

    генерация матрицы и возведение в квадрат элементов с четными индексами

---
+++ clwork002/Program.cs
@@ -0,0 +1,50 @@
+﻿int[,] GetArray(int m, int n, int minValue, int maxValue) {
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); ++i)
+    {
+        for (int j = 0; j < array.GetLength(1); ++j)
+        {
+            Console.Write(" " + array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] result = GetArray(rows, columns, 0, 9);
+PrintArray(result);
+
+void PrintNewArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); ++i)
+    {
+        for (int j = 0; j < array.GetLength(1); ++j)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] = array[i, j] * array[i, j];
+            }
+            Console.Write(" " + array[i, j]);
+        }
+     Console.WriteLine();
+    }
+}
+Console.WriteLine();
+PrintNewArray(result);
