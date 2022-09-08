92a1c03938af3384f81e94cd8edf335521a2fb30
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-09 01:52:37 +0600

    генерация матрицы и вывод суммы диагональных элементов

---
+++ clwork003/Program.cs
@@ -0,0 +1,41 @@
+﻿int[,] GetArray(int m, int n, int minValue, int maxValue) {
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; ++i)     {
+        for (int j = 0; j < n; ++j)         {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array) {
+    for (int i = 0; i < array.GetLength(0); ++i)     {
+        for (int j = 0; j < array.GetLength(1); ++j)         {
+            Console.Write(" " + array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] result = GetArray(rows, columns, 0, 9);
+PrintArray(result);
+
+Console.WriteLine();
+
+int sum = 0;
+Console.WriteLine ("Элементы диагонали массива: ");
+for (int i = 0; i < result.GetLength(0); ++i) {
+    for (int j = 0; j < result.GetLength(1); ++j)    {         
+        if (i == j)         {
+        sum += result[i, j];
+        Console.Write(" " + result[i,j]);
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Сумма элементов диагонали: " + sum);
