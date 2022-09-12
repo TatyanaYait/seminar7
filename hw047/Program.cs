27139a608d2864e7f01ca2a913326d8aa12c431c
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-12 15:54:08 +0600

    введено ограничение чисел после запятой

--- hw047/Program.cs
+++ hw047/Program.cs
@@ -14,7 +14,7 @@ void PrintArray(double[,] array)
 {
   for (int i = 0; i < array.GetLength(0); ++i) {
     for (int j = 0; j < array.GetLength(1); ++j) {
-      Console.Write("  " + array[i, j]);
+      Console.Write("  " + Math.Round(array[i, j],2));
     }
     Console.WriteLine();
   }
